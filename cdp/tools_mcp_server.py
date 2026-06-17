cc03c2d785d4379d269731f0da14447602496f7c
jaifiresong <boydearea@163.com>
2026-06-17 23:27:48 +0800

    test

--- cdp/tools_mcp_server.py
+++ cdp/tools_mcp_server.py
@@ -4,43 +4,11 @@ import sys
 
 sys.path.append(sys.path[0])
 
-from tools import (
-    browser_open,
-    browser_snapshot,
-    browser_extract,
-    browser_click,
-    browser_fill,
-    browser_type,
-    browser_scroll,
-    browser_scroll_to_bottom,
-    browser_scroll_into_view,
-    browser_evaluate,
-    browser_title,
-    browser_current_url,
-    browser_wait,
-    browser_close,
-)
+from tools import BROWSER_TOOLS
 
 mcp = FastMCP("基于CDP操作浏览器")
 
-_LANGCHAIN_TOOLS = [
-    browser_open,
-    browser_snapshot,
-    browser_extract,
-    browser_click,
-    browser_fill,
-    browser_type,
-    browser_scroll,
-    browser_scroll_to_bottom,
-    browser_scroll_into_view,
-    browser_evaluate,
-    browser_title,
-    browser_current_url,
-    browser_wait,
-    browser_close,
-]
-
-for t in _LANGCHAIN_TOOLS:
+for t in BROWSER_TOOLS:
     mcp.add_tool(t.coroutine, name=t.name, description=t.description)
 
 if __name__ == '__main__':
